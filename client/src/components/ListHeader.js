1de8135455aad9f74b6e362cea6e632de1851461
Tou Xiong <touyeexiong@gmail.com>
2023-12-19 19:23:01 -0600

    styled ListHeader

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const ListHeader = ({listName}) => {
+
+    const signOut = () => {
+        console.log('signout');
+    }
+
   return (
     <div className='list-header'>
       <h1>{listName}</h1>
+      <div className='button-container'>
+        <button className='create'>Add New</button>
+        <button className='signout' onClick={signOut}>SIGN OUT</button>
+      </div>
     </div>
   )
 }
