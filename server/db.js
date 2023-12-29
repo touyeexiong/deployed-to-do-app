3abf7a4c823f3d7a5740ee29abb1aec3d08a137f
Tou Xiong <touyeexiong@gmail.com>
2023-12-28 20:37:59 -0600

    created .env file as setup db.js. Going to test if it works now.

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    user: "touyeexiong",
-    password: '',
-    host: 'localhost',
-    port: 5432,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port:  process.env.DBPORT,
     database: 'todoapp'
 })
 
