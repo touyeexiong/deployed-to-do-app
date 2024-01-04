cda9c2ebdac5bbeb1f46a33a8c47b098a96f91d6
Tou Xiong <touyeexiong@gmail.com>
2024-01-03 20:58:49 -0600

    added buttons to ListItem

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import TickIcon from "./TickIcon";
+import ProgressBar from "./ProgressBar";
 
-const ListItem = ({task}) => {
+const ListItem = ({ task }) => {
   return (
-    <div className='list-item'>
-      <p>{task.title}</p>
-    </div>
-  )
-}
+    <div className="list-item">
 
-export default ListItem
+      <div className="info-container">
+        <TickIcon />
+        <p className="task-title">{task.title}</p>
+        <ProgressBar />
+      </div>
+      <div className="button-container">
+      </div>
+      <button className="edit">EDIT</button>
+      <button className="delete">DELETE</button>
+    </div>
+  );
+};
+
+export default ListItem;
