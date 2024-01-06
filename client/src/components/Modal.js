89c5de11da119056094d29d0b8bf2f8701cb88d6
Tou Xiong <touyeexiong@gmail.com>
2024-01-05 20:38:18 -0600

    setting up Modal component with inputs and onChange

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,9 +1,44 @@
 import React from 'react'
 
 const Modal = () => {
+
+  const handleChange = () => {
+    console.log('changing');
+    
+  }
+
+  const mode = 'create'
   return (
-    <div>
-      
+    <div className='overlay'>
+      <div className='modal'>
+        <div className='form-title-container'>
+          <h3>Let's {mode} your task</h3>
+          <button>X</button>
+        </div>
+
+          <form>
+            <input 
+              required
+              maxLength={30}
+              placeholder='Your task goes here'
+              name='title'
+              value={''}
+              onChange={handleChange}
+            />
+            <br/>
+            <input 
+              required
+              type='range'
+              min="0"            
+              max="100"
+              name="progress"
+              value={""}
+              onChange={handleChange} 
+              />
+            <input className={mode} type='submit'/>
+          </form>
+
+      </div>
     </div>
   )
 }
