b1a14b6b6b7a7d32a77b154a663344ea0531d636
Tou Xiong <touyeexiong@gmail.com>
2024-01-08 20:12:34 -0600

    modal now taking value of progress and task title.

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -37,7 +37,7 @@ const Modal = () => {
               maxLength={30}
               placeholder='Your task goes here'
               name='title'
-              value={''}
+              value={data.title}
               onChange={handleChange}
             />
             <br/>
@@ -49,7 +49,7 @@ const Modal = () => {
               min="0"            
               max="100"
               name="progress"
-              value={""}
+              value={data.progress}
               onChange={handleChange} 
               />
             <input className={mode} type='submit'/>
