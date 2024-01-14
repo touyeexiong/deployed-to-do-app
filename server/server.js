10507a1247507f6fb41394a940f7c42f632003f6
Tou Xiong <touyeexiong@gmail.com>
2024-01-13 23:37:03 -0600

    setup post route in server.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const PORT = process.env.PORT ?? 8000;
 const express = require("express");
+// const {v4:uuid}git af
 const app = express();
 const pool = require("./db");
 const cors = require("cors");
@@ -23,5 +24,17 @@ app.get(`/todos/:userEmail`, async (req, res) => {
     console.log(error);
   }
 });
+
+// create a new todo
+
+app.post('/todos', (req, res) =>{
+  const {user_email, title, progress, date} = req.body;
+  try {
+    pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)`, [id, user_email, title, progress, date])
+  } catch (error) {
+    console.log(error);
+    
+  }
+})
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
