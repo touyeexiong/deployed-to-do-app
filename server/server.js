0e6f73d833420d0dc60160840e81f58dea4ff967
Tou Xiong <touyeexiong@gmail.com>
2024-01-15 21:04:37 -0600

    added uuidv4 to server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT ?? 8000;
 const express = require("express");
-// const {v4:uuid}git af
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 const pool = require("./db");
 const cors = require("cors");
@@ -10,7 +10,6 @@ app.use(cors());
 // get all todos
 
 app.get(`/todos/:userEmail`, async (req, res) => {
-
   const { userEmail } = req.params;
   console.log(userEmail);
 
@@ -27,14 +26,17 @@ app.get(`/todos/:userEmail`, async (req, res) => {
 
 // create a new todo
 
-app.post('/todos', (req, res) =>{
-  const {user_email, title, progress, date} = req.body;
+app.post("/todos", (req, res) => {
+  const { user_email, title, progress, date } = req.body;
+  const id = uuidv4();
   try {
-    pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)`, [id, user_email, title, progress, date])
+    pool.query(
+      `INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)`,
+      [id, user_email, title, progress, date]
+    );
   } catch (error) {
     console.log(error);
-    
   }
-})
+});
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
