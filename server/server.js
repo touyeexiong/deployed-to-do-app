0d0266254f8e0d4d3ac71fcd869389fa69a2e33a
Tou Xiong <touyeexiong@gmail.com>
2024-01-22 21:02:57 -0600

    showing response from post in res.json()

--- server/server.js
+++ server/server.js
@@ -28,16 +28,17 @@ app.get(`/todos/:userEmail`, async (req, res) => {
 
 // create a new todo
 
-app.post("/todos", (req, res) => {
+app.post("/todos", async (req, res) => {
   const { user_email, title, progress, date } = req.body;
   console.log(user_email, title, progress, date);
 
   const id = uuidv4();
   try {
-    pool.query(
+    const newTodo = await pool.query(
       `INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)`,
       [id, user_email, title, progress, date]
     );
+    res.json(newTodo);
   } catch (error) {
     console.log(error);
   }
